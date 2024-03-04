878075831e979772cbb7b6dcb16dea85eb04d507
Staryy-Dobryy <lfcega1@gmail.com>
2024-03-04 23:00:07 +0200

    Created registration handler for user model

---
+++ src/main/java/com/mailsender/domain/model/user/RegistrationHandler.java
@@ -0,0 +1,33 @@
+package com.mailsender.domain.model.user;
+
+import com.mailsender.domain.model.user.exceptions.EmailAddressExistsException;
+import com.mailsender.domain.model.user.exceptions.RegistrationException;
+import com.mailsender.domain.model.user.exceptions.UsernameExistsException;
+import com.mailsender.infrastructure.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RegistrationHandler {
+    private UserRepository repository;
+
+    public RegistrationHandler(UserRepository repository) {
+        this.repository = repository;
+    }
+    public UserModel Register(String username, String email) throws RegistrationException {
+
+        UserModel existingUser = repository.findByUsername(username);
+        if (existingUser != null) {
+            throw new UsernameExistsException();
+        }
+
+        existingUser = repository.findByEmail(email.toLowerCase());
+        if (existingUser != null) {
+            throw new EmailAddressExistsException();
+        }
+
+
+        UserModel newUser = new UserModel(username, email);
+        repository.save(newUser);
+        return newUser;
+    }
+}
